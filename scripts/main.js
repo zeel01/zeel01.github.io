cba0b9a10d22353d96b77f5aa48d250e1466cb26
zeel <zeel02@gmail.com>
2021-07-19 02:44:40 -0400

    Added image lightbox

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,48 @@
+// Add mouseover and mouseout event listeners to links with data-preview attribute
 document.querySelectorAll("[data-preview]")
 	.forEach((element) => {
-		element.addEventListener("mouseover", onMouseOver);
-		element.addEventListener("mouseout", onMouseOut);
+		element.addEventListener("mouseover", showPreview);
+		element.addEventListener("mouseout", hidePreview);
 	});
 
-function onMouseOver(event) {
+// Add click event to all images
+document.querySelectorAll("img[data-lightbox]")
+	.forEach((element) => {
+		element.addEventListener("click", showLightbox);
+	});
+
+// Add clich event to close lightbox
+document.querySelector("#lightbox")
+	.addEventListener("click", hideLightbox);
+
+// Show preview image on hover
+function showPreview(event) {
 	const el = event.currentTarget;
 	const src = el.dataset.preview;
 	const preview = document.querySelector("#preview");
 
 	preview.style.backgroundImage = `url(${src})`;
 }
-function onMouseOut() {
+
+// Hide preview image on mouseout
+function hidePreview() {
 	const preview = document.querySelector("#preview");
 	preview.style.backgroundImage = "";
+}
+
+// Dsiplay lightbox on click
+function showLightbox(event) {
+	const el = event.currentTarget;
+	const src = el.src;
+	const lightbox = document.querySelector("#lightbox");
+	const figure = document.querySelector("#lightbox figure");
+
+	figure.style.backgroundImage = `url(${src})`;
+	lightbox.style.display = "grid";
+}
+
+// Hide lightbox on click
+function hideLightbox(event) {
+	const lightbox = event.currentTarget;
+	lightbox.style.display = "none";
 }
